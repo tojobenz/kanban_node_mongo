c15e6867742085153db5a6676733a9377057200c
Aina Tojosoa <ainatojosoa@gmail.com>
2026-09-13 07:35:01 +0200

    fix(cors): set production origin fallback

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:8081"
+  origin: process.env.FRONTEND_URL || 
+    (process.env.NODE_ENV === 'production' 
+      ? 'https://kanban-vue-front.vercel.app' 
+      : 'http://localhost:8081')
 };
 
 app.use(cors(corsOptions));
