2cd56215bdaa273a72262da4db1c938b6e78776d
Aina Tojosoa <ainatojosoa@gmail.com>
2026-09-13 09:18:47 +0200

    fix: user

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // MongoDB Atlas 
-  URI: process.env.MONGODB_URI || "mongodb+srv://<username>:<password>@cluster.mongodb.net/kanban?retryWrites=true&w=majority"
+  URI: process.env.MONGODB_URI || "mongodb+srv://votre_username:votre_password@votre_cluster.mongodb.net/kanban?retryWrites=true&w=majority"
 };
